91fb9eaecb1ed2f6285282184e1aa0af05d609ff
rakesh <rakeshreddy.k5777@gmail.com>
2024-04-30 00:32:10 +0530

    Add bubbleSort and selectionSort algorithms to Sort directory

---
+++ Sort/bubbleSort.js
@@ -0,0 +1,28 @@
+// push the maximum number to left with ADJACENT SWAPS 
+// we can optimize the flow by breaking loop if there are no swaps, which means array is sorted
+// make sure while doing adjacent swaps, we have to take care of last element because [1,3,2], i should only check till 3 not till 2
+
+function bubbleSort(numsArray) {
+    for (let i = numsArray.length - 1; i >= 1; i--) {
+        let didSwap = false;
+        for (let j = 0; j <= i - 1; j++) {// i - 1 is required because [1,3,2], i should only check till 3 not till 2
+            if (numsArray[j] > numsArray[j + 1]) {
+                swap(numsArray, j, j + 1);
+                didSwap = true;
+            }
+        }
+        if (!didSwap) {
+            break; // this didSwap is to optimise best case, if no swaps are done, which means array is sorted and no need to do further checks and swaps
+        }
+        console.log('ran')
+    }
+    return numsArray;
+}
+
+function swap(array, first, second) {
+    const temp = array[first];
+    array[first] = array[second];
+    array[second] = temp;
+}
+
+console.log(bubbleSort([1, 8, 2, 2, 2, 2]));
--- Sort/selectionSort.js
+++ Sort/selectionSort.js
@@ -1,18 +1,22 @@
+//select the minimum number in the array and swap with current index, and loop every index and make sure to ignore already sorted part in right
+//At the current index, select the minimum element from all elements in the LEFT of the array and SWAP
+
 function selectionSort(numsArray) {
-    let i = 0; j = 0;
-    for (i = 0; i < numsArray.length - 1; i++) { //length - 1, as no need to check last element of array
+    for (let i = 0; i < numsArray.length - 1; i++) { //length - 1, as no need to check last element of array
         let minValueIndex = i;
-        for (j = i; j < numsArray.length; j++) {
+        for (let j = i; j < numsArray.length; j++) {
             if (numsArray[j] < numsArray[minValueIndex]) minValueIndex = j;
         }
-        if (minValueIndex != i) {
-            const minValue = numsArray[minValueIndex];
-            numsArray[minValueIndex] = numsArray[i];
-            numsArray[i] = minValue;
-        }
+        if (minValueIndex != i) swap(numsArray, minValueIndex, i);
     }
 
     return numsArray;
+}
+
+function swap(array, first, second) {
+    const temp = array[first];
+    array[first] = array[second];
+    array[second] = temp;
 }
 
 console.log(selectionSort([1, 3, 2]));
