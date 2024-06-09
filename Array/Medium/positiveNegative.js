9b546b9a76305df02dbc651449f96d48b9d2af2a
rakesh reddy <rakeshreddy.k5777@gmail.com>
2024-06-09 16:06:36 +0530

    feat: Rearrange positive and negative numbers in array

--- Array/Medium/positiveNegative.js
+++ Array/Medium/positiveNegative.js
@@ -1,3 +1,5 @@
+// Problem Statement: There’s an array ‘A’ with an equal number of positive and negative elements. Without altering the relative order of positive and negative elements, you must return an array of alternately positive and negative values.
+
 var rearrangeArray = function (nums) {
     let firstPointer = 0; // use this to find positive 
     let secondPointer = 0; // use this to find negative
@@ -5,11 +7,11 @@ var rearrangeArray = function (nums) {
 
     while (firstPointer < nums.length && secondPointer < nums.length) {
 
-        while (nums[firstPointer] < 0) firstPointer++;
-        while (nums[secondPointer] > 0) secondPointer++;
+        while (nums[firstPointer] < 0) firstPointer++; // find next postive number
+        while (nums[secondPointer] >= 0) secondPointer++; // find next negative number
 
-        if (firstPointer < nums.length) result.push(nums[firstPointer]);
-        if (secondPointer < nums.length) result.push(nums[secondPointer]);
+        if (firstPointer < nums.length) result.push(nums[firstPointer]); // push positive number
+        if (secondPointer < nums.length) result.push(nums[secondPointer]); // push negative number 
 
         firstPointer++;
         secondPointer++;
@@ -17,3 +19,5 @@ var rearrangeArray = function (nums) {
 
     return result;
 };
+
+console.log(rearrangeArray([1, 2, -1, 3, 1, -4, 5, 6, -3, -1, -2, 5]));
