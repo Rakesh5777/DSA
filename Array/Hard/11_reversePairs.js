46aad9e5a40690eb4b15575d3ef757f7133c1ec7
rakesh reddy <rakeshreddy.k5777@gmail.com>
2024-06-14 00:45:42 +0530

    feat: Add reversePairs and maxProduct algorithms

---
+++ Array/Hard/11_reversePairs.js
@@ -0,0 +1,62 @@
+// Problem Statement: Given an array of numbers, you need to return the count of reverse pairs. Reverse Pairs are those pairs where i<j and arr[i]>2*arr[j].
+// this is same like count inversions, but we have to add simple logic to count reverse pairs in sort and merge function
+var reversePairs = function (nums) {
+    if (nums.length === 0) return 0;
+    return merge(nums, 0, nums.length - 1);
+};
+
+function merge(nums, low, high) {
+    let count = 0;
+    if (low < high) {
+        let mid = Math.floor((low + high) / 2);
+        count += merge(nums, low, mid);
+        count += merge(nums, mid + 1, high);
+        count += sortAndMerge(nums, low, mid, high);
+    }
+    return count;
+}
+
+function sortAndMerge(nums, low, mid, high) {
+    let left = low;
+    let right = mid + 1;
+    let temp = [];
+    let count = 0;
+
+    // Count reverse pairs
+    while (left <= mid) { // as we have traverse full left side array
+        while (right <= high && nums[left] > 2 * nums[right]) right++; // to find the index where condition doesn't match
+        count += (right - (mid + 1));
+        left++;
+    }
+
+    // Reset pointers for merging
+    left = low;
+    right = mid + 1;
+
+    // Merge the two sorted halves
+    while (left <= mid && right <= high) {
+        if (nums[left] <= nums[right]) {
+            temp.push(nums[left]);
+            left++;
+        } else {
+            temp.push(nums[right]);
+            right++;
+        }
+    }
+
+    while (left <= mid) {
+        temp.push(nums[left]);
+        left++;
+    }
+
+    while (right <= high) {
+        temp.push(nums[right]);
+        right++;
+    }
+
+    for (let i = 0; i < temp.length; i++) {
+        nums[low + i] = temp[i];
+    }
+
+    return count;
+}
