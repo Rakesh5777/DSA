8748bde040308bb42327dffa3b1661e573acd21d
rakesh reddy <rakeshreddy.k5777@gmail.com>
2024-06-14 00:45:30 +0530

    Fix typo in comment for countInversions.js

--- Array/Hard/10_countInversions.js
+++ Array/Hard/10_countInversions.js
@@ -1,6 +1,6 @@
 // Problem Statement: Given an array of N integers, count the inversion of the array (using merge-sort).
 // we will directly use merge sort to collect count while merging 
-// Idea is while merging two SORTED ARRAYS we can easily count the inersions 
+// Idea is while merging two SORTED ARRAYS we can easily count the inversion 
 // so in merge sort, while merging we will count
 // counting logic is simple, [2, 3] [1]
 // when i merging this two, when i' m comparing 2 with 1, 2 is greater than 1, so as [2, 3] is sorted, `EVERYTHING IN THIS ARRAY AFTER 2` will also be greater than 1
