dc1cf8885dfce08f6cb7f7c6763649e2a37ae65a
rakesh reddy <rakeshreddy.k5777@gmail.com>
2024-11-07 21:04:45 +0530

    feat: Add convertArrayToLL function to convert an array into a linked list

---
+++ 6_LinkedList/1_1D_Linked_list/1_convertArraytoLL.js
@@ -0,0 +1,24 @@
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+function convertArrayToLL(arr) {
+    let head = new Node(arr[0]);
+    let tail = new Node();
+    let temp = head;
+
+    for (let i = 1; i < arr.length; i++) {
+        let current = new Node(arr[i])
+        temp.next = current;
+        temp = current;
+    }
+    tail = temp;
+    return head;
+}
+
+console.log(JSON.stringify(convertArrayToLL([1, 2, 3, 4, 5])));
